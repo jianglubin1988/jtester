353f66434988c3cff8387743aa34cf70aa181126
jianglubin <jianglubin@trio.ai>
2017-11-27 14:10:48 +0800

    update

--- src/main/java/com/bin/thread/HttpThread.java
+++ src/main/java/com/bin/thread/HttpThread.java
@@ -17,6 +17,8 @@ public class HttpThread extends Thread {
 	public void run() {
 		try {
 			Set<Map<String, Object>> sets = PropertiesUtils.getAddrs();
+			Long temp = 0l;
+			String maxLog = "";
 			Long errCount = 0l;
 			Long total = 0l;
 			int i = 0;
@@ -24,23 +26,42 @@ public class HttpThread extends Thread {
 			while (i < count) {
 				for (Map<String, Object> map : sets) {
 					Long start = System.currentTimeMillis();
-					JSONObject param = map.get("param") == null ? null : (JSONObject) map.get("param");
+					JSONObject param = map.get("params") == null ? null : (JSONObject) map.get("params");
 					String url = (String) map.get("url");
-					HttpRequestUtils.httpPost(url, param);
+					String result = HttpRequestUtils.httpPost(url, param);
 					Long end = System.currentTimeMillis();
 					Long interval = end - start;
+					String normLog = "\n线程：" + Thread.currentThread().getName()
+							+ "\n接口：" + url
+							+ "\n耗时：" + interval + "ms"
+							+ "\n请求报文：" + param
+							+ "\n返回报文：" + result;
+					if(interval > temp) {
+						temp = interval;
+						maxLog = normLog;
+					}
 					total += interval;
 					if (interval > PropertiesUtils.getTimeout()) {
 						errCount++;
-						logger.info("线程" + Thread.currentThread().getName() + "调用Http接口" + url + "耗时：" + interval + "ms");
+						logger.info(normLog);
 					}
 				}
 				i++;
 			}
 			Long num = count * sets.size() * 1l;
 			Long average = total / num;
-			logger.info("线程名称：" + Thread.currentThread().getName() + "，Http请求数量： " + num + "，请求超时数量： " + errCount
-					+ "，总耗时: " + total + "ms，平均耗时： " + average + "ms");
+			Long max = temp;
+			logger.info("\n================线程【" + Thread.currentThread().getName() + "】信息开始================"
+					+ "\n总耗时: " + total + "ms"
+					+ "\n平均耗时： " + average + "ms"
+					+ "\n最大耗时：" + max + "ms"
+					+ "\n请求数量： " + num
+					+ "\n超时数量： " + errCount
+					+ "\n----最大耗时信息----"
+					+ maxLog
+					+ "\n================线程【" + Thread.currentThread().getName() + "】信息结束================"
+					+ "\n\n"
+			);
 		} catch (Exception e) {
 			e.printStackTrace();
 			logger.info("调用接口失败！异常信息：" + e.getMessage());
